4985cd58c2c659429e8eeaf1f8a31861ff04b2c3
karlyabut <karleisenyabut@gmail.com>
2019-10-26 19:18:42 +0000

    Canvas drawing added (still need to figure out drawing on mobile view)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Canvas from './components/Canvas'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h3 style={{ textAlign: 'center' }}>DRAWw BITCH</h3>
+      <Canvas />
+    </Fragment>
   );
 }
 
--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,1 +1,104 @@
-import React from "react";
+import React, { Component } from 'react';
+import Pusher from 'pusher-js';
+
+class Canvas extends Component {
+  constructor(props) {
+    super(props);
+    this.onMouseDown = this.onMouseDown.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onTouchMove = this.onTouchMove.bind(this);
+    this.endPaintEvent = this.endPaintEvent.bind(this);
+    this.pusher = new Pusher('b37e8b05f23c407c454e', {
+      cluster: 'us2',
+    });
+  }
+
+  isPainting = false;
+  // Different stroke styles to be used for user and guest
+  userStrokeStyle = '#000';
+  // guestStrokeStyle = '#F0C987';
+  line = [];
+  prevPos = { offsetX: 0, offsetY: 0 };
+
+  onMouseDown({ nativeEvent }) {
+    const { offsetX, offsetY } = nativeEvent;
+    this.isPainting = true;
+    this.prevPos = { offsetX, offsetY };
+  }
+
+  onMouseMove({ nativeEvent }) {
+    if (this.isPainting) {
+      const { offsetX, offsetY } = nativeEvent;
+      const offSetData = { offsetX, offsetY };
+      // Set the start and stop position of the paint event.
+      const positionData = {
+        start: { ...this.prevPos },
+        stop: { ...offSetData },
+      };
+      // Add the position to the line array
+      this.line = this.line.concat(positionData);
+      this.paint(this.prevPos, offSetData, this.userStrokeStyle);
+    }
+  }
+
+  onTouchMove({ nativeEvent }) {
+    if (this.isPainting) {
+      const { offsetX, offsetY } = nativeEvent;
+      const offSetData = { offsetX, offsetY };
+      // Set the start and stop position of the paint event.
+      const positionData = {
+        start: { ...this.prevPos },
+        stop: { ...offSetData },
+      };
+      // Add the position to the line array
+      this.line = this.line.concat(positionData);
+      this.paint(this.prevPos, offSetData, this.userStrokeStyle);
+    }
+  }
+
+  endPaintEvent() {
+    if (this.isPainting) {
+      this.isPainting = false;
+    }
+  }
+  paint(prevPos, currPos, strokeStyle) {
+    const { offsetX, offsetY } = currPos;
+    const { offsetX: x, offsetY: y } = prevPos;
+
+    this.ctx.beginPath();
+    this.ctx.strokeStyle = strokeStyle;
+    // Move the the prevPosition of the mouse
+    this.ctx.moveTo(x, y);
+    // Draw a line to the current position of the mouse
+    this.ctx.lineTo(offsetX, offsetY);
+    // Visualize the line using the strokeStyle
+    this.ctx.stroke();
+    this.prevPos = { offsetX, offsetY };
+  }
+
+  componentDidMount() {
+    // Here we set up the properties of the canvas element. 
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+    this.ctx = this.canvas.getContext('2d');
+    this.ctx.lineJoin = 'round';
+    this.ctx.lineCap = 'round';
+    this.ctx.lineWidth = 5;
+  }
+
+  render() {
+    return (
+      <canvas
+      // We use the ref attribute to get direct access to the canvas element. 
+        ref={(ref) => (this.canvas = ref)}
+        style={{ background: 'blue' }}
+        onMouseDown={this.onMouseDown}
+        onTouchMove={this.onTouchMove}
+        onMouseLeave={this.endPaintEvent}
+        onMouseUp={this.endPaintEvent}
+        onMouseMove={this.onMouseMove}
+      />
+    );
+  }
+}
+export default Canvas;
