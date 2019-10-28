06f77928c11b5f587acc32cd342ab6e5aa00d7b3
malloryminchau <mallory.m.minchau@gmail.com>
2019-10-28 11:11:09 -0400

    websocket research

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,3 +7,61 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+/*
+THIS IS THE SERVER
+
+const http = require('http');
+const static = require('node-static');
+const file = new static.Server('./');
+const server = http.createServer((req, res) => {
+  req.addListener('end', () => file.serve(req, res)).resume();
+});
+const port = 3210;
+server.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+
+HTML
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>WebSocket Example</title>
+    <script src="client.js"></script>
+  </head>
+  <body>
+    <h1>WebSocket Example</h1>
+    <p>Open your browser's developer tools to see client/server activity.</p>
+  </body>
+</html>
+
+REPLACE SOME OF THE SERVER CODE
+
+
+const http = require('http');
+const crypto = require('crypto');
+const static = require('node-static');
+const file = new static.Server('./');
+const server = http.createServer((req, res) => {
+  req.addListener('end', () => file.serve(req, res)).resume();
+});
+server.on('upgrade', function (req, socket) {
+  if (req.headers['upgrade'] !== 'websocket') {
+    socket.end('HTTP/1.1 400 Bad Request');
+    return;
+  }
+  // Read the websocket key provided by the client: const acceptKey = req.headers['sec-websocket-key']; // Generate the response value to use in the response: const hash = generateAcceptValue(acceptKey); // Write the HTTP response into an array of response lines: const responseHeaders = [ 'HTTP/1.1 101 Web Socket Protocol Handshake', 'Upgrade: WebSocket', 'Connection: Upgrade', `Sec-WebSocket-Accept: ${hash}` ]; // Write the response back to the client socket, being sure to append two // additional newlines so that the browser recognises the end of the response // header and doesn't continue to wait for more header data: socket.write(responseHeaders.join('\r\n') + '\r\n\r\n');
+});
+ // Don't forget the hashing function described earlier:
+function generateAcceptValue (acceptKey) {
+  return crypto
+  .createHash('sha1')
+  .update(acceptKey + '258EAFA5-E914–47DA-95CA-C5AB0DC85B11', 'binary')
+  .digest('base64');
+}
+
+
+
+
+*/
