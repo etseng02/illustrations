ec016f7cc7db46e3f36413a0fc2633f1453b6ad4
Eddie <eddietseng@outlook.com>
2019-10-28 11:11:33 -0400

    added hostMachine component

--- src/App.js
+++ src/App.js
@@ -3,27 +3,51 @@ import './App.css';
 import Canvas from './components/Canvas'
 import Button from './components/Button'
 import JoinRoom from './components/JoinRoom'
+import HostRoom from './components/HostRoom'
+
 
 function App() {
   
   const [state, setState] = useState({
     roomID: "",
-    playerPosition: null,
     name: "",
-    hostMachine: false
+    playerPosition: null,
+    keyword: "",
+    hostMachine: false,
+    phase: "",
   });
 
   function enterRoom(name, room){
     setState({ ...state, name: name, roomID: room });
   }
 
-  function createRoom(){
-    console.log("Room ID Creating Now")
-    setState({ ...state, hostMachine: true })
-  }
+  
+  const generateRandomString = function() {
+    const possibleCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let newShortURL = "";
+    while (newShortURL.length < 4) {
+      newShortURL = newShortURL + possibleCharacters[Math.floor(Math.random() * 36)];
+    }
+    return newShortURL;
+  };
 
+  function createRoom(){
+    let roomCode = generateRandomString()
+    setState({ ...state, hostMachine: true, roomID: roomCode })
+  }
+  
   return (
     <Fragment>
+
+      {state.roomID && state.hostMachine &&
+      <Fragment>
+        <HostRoom
+          roomID={state.roomID}
+        >
+        </HostRoom>
+
+      </Fragment>
+      }
 
       {!state.roomID && !state.hostMachine &&//When roomID is falsy, Join room field, name field, and create room field will be rendered
         <Fragment>
@@ -37,9 +61,9 @@ function App() {
        </Fragment>
       }
 
-      {state.roomID && !state.hostMachine && //
+      {state.roomID && !state.hostMachine && //Draw Phase
       <Fragment>
-        <h3 style={{ textAlign: 'center' }}>Draw</h3>
+        <h3 style={{ textAlign: 'center' }}>Draw Phase</h3>
         <Canvas />
       </Fragment>
       }
