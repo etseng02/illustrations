f83fd3a8b782ae1590aa4c119dbcf621ee876032
malloryminchau <mallory.m.minchau@gmail.com>
2019-10-28 20:27:27 -0400

    changed the port number in both app files

--- api/app.js
+++ api/app.js
@@ -50,7 +50,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(80);
+server.listen(8080);
 // WARNING: app.listen(80) will NOT work here!
 
 app.get('/', function (req, res) {
