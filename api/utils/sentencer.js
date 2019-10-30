fd35316bbbec8c2bd3b72c0355a425926cc6988e
malloryminchau <mallory.m.minchau@gmail.com>
2019-10-30 16:54:44 -0400

    added prompt generator using sentencer library

---
+++ api/utils/sentencer.js
@@ -0,0 +1,25 @@
+// new word generator using Sentencer library
+var Sentencer = require('sentencer');
+
+Sentencer.configure({
+  // the list of nouns to use.
+  nounList: ['cat', 'dog', 'mouse', 'horse', 'car', 'ghost', 'cow', 'bug', 'sock', 'snake', 'skateboard', 'apple', 'drum', 'cup', 'house', 'cheese',
+  'beer', 'ability', 'village', 'tea', 'grandmother', 'historian', 'desk', 'bacon'],
+  // the list of adjectives to use.
+  adjectiveList: ['slimy', 'furry', 'scaley', 'sore', 'anxious', 'sorry', 'famous', 'serious', 'educational', 'unhappy', 'terrible', 'embarrassed',
+  'environmental', 'powerful', 'historical', 'serious', 'pleasant', 'poor', 'boring', 'remarkable', 'important', 'lucky', 'basic', 'old', 'electronic', 'strong'],
+  actions: {
+    my_action: function(){
+      return "something";
+    }
+  }
+});
+
+
+function prompt () {
+  var noun = Sentencer.make("{{ noun }}");
+  var adjective = Sentencer.make("{{ an_adjective }}");
+  return adjective + " " + noun;
+}
+
+module.exports = prompt;
