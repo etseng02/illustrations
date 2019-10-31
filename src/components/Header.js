dcb3054c6d7ee41b7f0e2e9360441780219c4ba3
Eddie <eddietseng@outlook.com>
2019-10-31 12:06:58 -0400

    Added pattern to header and moved header css to a different component css file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 
+import "./Header.css";
+
 export default function Header(props) {
 
   return (
     <Fragment>
       <header>
-        <h1 id="logo">Illustrations</h1>
+        <h1 id="logo">Illustrations!</h1>
         <h1 id="room">{props.room}</h1>
       </header>
 
