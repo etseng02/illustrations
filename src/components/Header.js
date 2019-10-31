4a51871ea3669f94a61bcee73549f2bff835945f
malloryminchau <mallory.m.minchau@gmail.com>
2019-10-31 14:55:47 -0400

    name change

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export default function Header(props) {
   return (
     <Fragment>
       <header>
-        <h1 id="logo">Illustrations!</h1>
+        <h1 id="logo">Skedoodle!</h1>
         <h1 id="room">{props.room}</h1>
       </header>
 
