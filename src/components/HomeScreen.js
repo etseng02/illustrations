46e3eddb9e1819cec4163ada5c70dec13af0ef58
Eddie <eddietseng@outlook.com>
2020-07-13 23:12:46 -0400

    Restyling home screen and menu

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,45 +1,50 @@
 import React, { useState } from "react"
+import Menu from "./common/Menu"
 
-export default function HomeScreen(props) {
-  const [name, setName] = useState(props.name || "")
-  const [room, setRoom] = useState(props.name || "")
+const HomeScreen = () => {
+  //   const [name, setName] = useState(props.name || "")
+  //   const [room, setRoom] = useState(props.name || "")
 
-  function validate() {
-    if (name === "") {
-      console.log("Name cannot be blank")
-      return
-    }
-    if (room === "") {
-      console.log("Room ID cannot be blank")
-      return
-    }
-    props.onClick(name, room)
-  }
+  //   function validate() {
+  //     if (name === "") {
+  //       console.log("Name cannot be blank")
+  //       return
+  //     }
+  //     if (room === "") {
+  //       console.log("Room ID cannot be blank")
+  //       return
+  //     }
+  //     props.onClick(name, room)
+  //   }
 
   return (
-    <form autoComplete="off">
-      <div class="flex flex-col bg-gray-200 m-auto mt-16 w-3/5 lg:w-4/12 p-6 space-y-4">
-        <input
-          id="name-field"
-          value={name}
-          type="text"
-          placeholder="Enter your name"
-          onChange={event => setName(event.target.value)}
-        />
-        <input
-          id="room-field"
-          value={room}
-          type="text"
-          placeholder="Enter Room ID"
-          onChange={event => setRoom(event.target.value)}
-        />
-        <button id="join-room" onClick={() => validate()}>
-          Join Room
-        </button>
-        <button id="create-room" onClick={() => props.createRoom()}>
-          Create Room
-        </button>
-      </div>
-    </form>
+    <Menu />
+
+    // <form autoComplete="off">
+    //   <div class="flex flex-col bg-gray-200 m-auto mt-16 w-3/5 lg:w-4/12 p-6 space-y-4">
+    //     <input
+    //       id="name-field"
+    //       value={name}
+    //       type="text"
+    //       placeholder="Enter your name"
+    //       onChange={event => setName(event.target.value)}
+    //     />
+    //     <input
+    //       id="room-field"
+    //       value={room}
+    //       type="text"
+    //       placeholder="Enter Room ID"
+    //       onChange={event => setRoom(event.target.value)}
+    //     />
+    //     <button id="join-room" onClick={() => validate()}>
+    //       Join Room
+    //     </button>
+    //     <button id="create-room" onClick={() => props.createRoom()}>
+    //       Create Room
+    //     </button>
+    //   </div>
+    // </form>
   )
 }
+
+export default HomeScreen
---
+++ src/components/common/Menu.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react"
+
+const Menu = () => {
+  const [selectedMenuItem, setSelectedMenuItem] = useState("")
+  const [selectedSubMenuItem, SetSelectedSubMenuItem] = useState("")
+
+  return (
+    <>
+      <div class="bg-gray-200 m-auto mt-16 w-3/5 lg:w-4/12 p-6 ">
+        <button onClick={() => {}}>Back</button>
+        <div class="flex flex-col space-y-4">
+          {selectedMenuItem === "" && (
+            <>
+              <button onClick={() => setSelectedMenuItem("play")}>Play</button>
+              <button>How to Play</button>
+              <button>Settings</button>
+              <button>About</button>
+            </>
+          )}
+
+          {selectedMenuItem === "play" && (
+            <>
+              <button id="join-room" onClick={() => {}}>
+                Join Room
+              </button>
+              <button id="create-room" onClick={() => {}}>
+                Create Room
+              </button>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Menu
