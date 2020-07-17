86f7985d0a89b6c65cc6a4204b700dac321f9992
Eddie <eddietseng@outlook.com>
2020-07-16 22:36:12 -0400

    Added back button to Menu and added types

--- src/components/common/Menu.js
+++ src/components/common/Menu.js
@@ -4,10 +4,18 @@ const Menu = () => {
   const [selectedMenuItem, setSelectedMenuItem] = useState("")
   const [selectedSubMenuItem, SetSelectedSubMenuItem] = useState("")
 
+  const handleBackButton = () => {
+    if (selectedSubMenuItem) {
+      SetSelectedSubMenuItem("")
+    } else {
+      setSelectedMenuItem("")
+    }
+  }
+
   return (
     <>
       <div class="bg-gray-200 m-auto mt-16 w-3/5 lg:w-4/12 p-6 ">
-        <button onClick={() => {}}>Back</button>
+        <button onClick={() => handleBackButton()}>Back</button>
         <div class="flex flex-col space-y-4">
           {selectedMenuItem === "" && (
             <>
@@ -18,7 +26,7 @@ const Menu = () => {
             </>
           )}
 
-          {selectedMenuItem === "play" && (
+          {selectedMenuItem === "play" && selectedSubMenuItem === "" && (
             <>
               <button id="join-room" onClick={() => {}}>
                 Join Room
